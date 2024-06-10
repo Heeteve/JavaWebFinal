fca0118489264f55e94cd0eebf6951f871f15d4a
Heeteve <a13267110880@qq.com>
2024-06-10 21:19:18 +0800

    fix: 修改管理员判断方式

--- src/main/java/com/javawebfinal/interceptor/AdminInterceptor.java
+++ src/main/java/com/javawebfinal/interceptor/AdminInterceptor.java
@@ -1,6 +1,8 @@
 package com.javawebfinal.interceptor;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.javawebfinal.util.JwtUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +17,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class AdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-        String url = req.getRequestURL().toString();
         // 获取Cookie
         Cookie[] cookies = req.getCookies();
         String jwt = null;
@@ -41,19 +42,21 @@ public class AdminInterceptor implements HandlerInterceptor {
         // 解析jwt，如果解析失败，返回错误结果（未登录）。
         try {
             JwtUtils.parseJWT(jwt);
-            // base64解析
+            // base64解码
             String[] split = jwt.split("\\.");
             String payload = new String(java.util.Base64.getDecoder().decode(split[1]));
-            // 检查是否为管理员
-            if (!payload.contains("\"role\":1")) {
-                log.warn("拦截请求：非管理员");
+            // JSON解析，判断是否为管理员
+            JSONObject jsonObject = JSON.parseObject(payload);
+            String role = jsonObject.getString("role");
+            if (!"1".equals(role)) {
+                log.warn("拦截请求：非管理员用户");
                 resp.setContentType("text/html;charset=utf-8");
-                resp.getWriter().write("非管理员，禁止访问");
+                resp.getWriter().write("非管理员用户，禁止访问");
                 return false;
             }
             
         } catch (Exception e) {//jwt解析失败
-            log.error("jwt解析失败", e);
+            log.warn("拦截请求：jwt解析失败", e);
             resp.setContentType("text/html;charset=utf-8");
             resp.getWriter().write("令牌错误，请重新登录");
             // 跳转登录页
